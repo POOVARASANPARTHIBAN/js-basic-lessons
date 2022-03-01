d19ce588cfb88632185d58dfd268dc230c6bc76c
Poovarasan P <poov3028@global.chainsys.com>
2022-03-01 20:07:16 +0530

    script commit

---
+++ src/sample2.js
@@ -0,0 +1,17 @@
+/* eslint-disable semi */
+const q = document.querySelector('q');
+
+const p = document.querySelector('p');
+
+/*
+ *  Using a variable without declaring the variable
+ */
+// town = "chennai"
+// console.log(town);
+
+const city = 'Chennai';
+console.log(city);
+const age = 100;
+console.log(q);
+console.log(p);
+console.log(age);
---
+++ src/sample4.js
@@ -0,0 +1,18 @@
+/* eslint-disable semi */
+console.log('Hi Node!!!');
+function callme () {
+  const x = 100;
+  const y = x * 2;
+  if (x < 500) {
+    console.log('x< 500');
+    const z = x + y;
+    console.log(`z : ${z}`);
+  }
+  if (y < 500) {
+    console.log('y<500');
+    const v1 = z + y;
+    console.log(`v1: ${v1}`);
+  }
+}
+
+console.log(callme());
